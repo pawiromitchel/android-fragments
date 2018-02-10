ff3b5fcbb7f44f11a281e4fbb3c17661bf55888d
Mitchel Pawirodinomo <pawiromitchel@hotmail.com>
2018-02-10 11:02:09 -0300

    Initial commit

---
+++ app/src/main/java/sr/unasat/fragmentsv2/MainActivity.java
@@ -0,0 +1,33 @@
+package sr.unasat.fragmentsv2;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void loadFragmentOne(View view) {
+        loadFragment(new FirstFragment());
+    }
+
+    public void loadFragmentTwo(View view) {
+        loadFragment(new SecondFragment());
+    }
+
+    public void loadFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment);
+        fragmentTransaction.commit();
+    }
+
+}
